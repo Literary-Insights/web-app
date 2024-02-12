c680b5d28903f3aa6821be6f3ae4406eb901985e
puppe1990 <matheus.puppe90@hotmail.com>
2024-02-12 01:15:39 -0300

    update rules

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,8 @@ class Article < ApplicationRecord
     has_many :article_categories
     has_many :categories, through: :article_categories
     has_one_attached :thumbnail
-    validates :title, presence: true, length: {minimum: 10, maximum: 120}
-    validates :description, presence:true, length: {minimum: 20, maximum: 250}
-    validates :content, presence:true
+    validates :title, presence: true, length: { minimum: 10, maximum: 120 }
+    validates :description, length: { maximum: 250 }
+    validates :content, presence: true
     attribute :views, :integer, default: 0
 end
